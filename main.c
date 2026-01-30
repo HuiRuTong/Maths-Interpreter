05570c1c1fd3e0eba7c3d1a32a279bf9205ab129
FishFlops <huirutong06@gmail.com>
2026-01-30 22:16:53 +0800

    Update main.c
    - Changed tokeniser to ignore spaces
    - Fixed some issues related to operator combinations
    - Added more errors
    - Added passive aggressive reminders

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ typedef enum {
     NUMERICAL,
     OPERATION,
     BRACKETS,
+    SPACE,
     END
 } TokenTypes;
 
@@ -23,6 +24,31 @@ typedef struct {
     size_t pos;
     Token current;
 } Interpreter;
+
+/*
+    Make an actual error handing function to pass these into; this is rlly ugly
+*/
+// Error types:
+void errMultipleDecimals() {
+    printf("You've put too many decimal points in a number.\n");
+    exit(1);
+}
+void errMissingOperand() {
+    printf("Seems you've forgotten something at the end.\n");
+    exit(1);
+}
+void errInvalidOperatorCombination() {
+    printf("No clue what you mean, bud.\n");
+    exit(1);
+}
+void errUndefinedSymbol() {
+    printf("You've entered a symbol I don't recognise.\n");
+    exit(1);
+}
+void errDivByZero() {
+    printf("Absolutely not!");
+    exit(1);
+}
 
 Token tokenise(Interpreter *interpreter) {
     /* 
@@ -42,21 +68,29 @@ Token tokenise(Interpreter *interpreter) {
     current_char[1] = '\0';
     strcpy(return_token.value, current_char);
 
-    if (isdigit(current_char[0])) {
-        return_token.type = NUMERICAL;
+    if (isdigit(current_char[0]) || current_char[0] == '.') {
+        return_token.type = NUMERICAL;\
         int decimals = 0;
+
+        if (current_char[0] == '.') {
+            decimals += 1;
+        }
 
         // Appends other digits onto the first
         while (isdigit((interpreter->equation)[(interpreter->pos) + 1]) ||
-               (interpreter->equation)[(interpreter->pos) + 1] == '.') {
+              (interpreter->equation)[(interpreter->pos) + 1] == '.' ||
+              (interpreter->equation)[(interpreter->pos) + 1] == ' ') {
             (interpreter->pos)++;
             current_char[0] = (interpreter->equation)[interpreter->pos];
 
-            if (!strcmp(current_char, ".") && ++decimals > 1) {
+            if (current_char[0] == ' ') {
+                continue;
+            } 
+
+            if (current_char[0] == '.' && ++decimals > 1) {
                 free(current_char);
                 free(return_token.value);
-                printf("You typed two decimal points in a number.");
-                exit(1);
+                errMultipleDecimals();
             }
             strcat(return_token.value, current_char);
         }
@@ -66,37 +100,55 @@ Token tokenise(Interpreter *interpreter) {
 
     if (ispunct(current_char[0])) {
         return_token.type = OPERATION;
-        int sign = 1;
+        int sign = 0;
+        int muldiv = 0;
 
         do {
-            if (!(strcmp(current_char, "+"))) {
-                sign *= 1;
-            } else if (!(strcmp(current_char, "-"))) {
-                sign *= -1;
-            } else {
-                // As long as there's an operator next to a * or /, reject it
-                if (ispunct((interpreter->equation)[(interpreter->pos) + 1]) ||
-                    ispunct((interpreter->equation)[(interpreter->pos) - 1])) {
-                    sign = 0;
-                    break;
+            if (current_char[0] != ' ') {
+                // If there's nothing after an operator
+                if (current_char[0] == '\n') {
+                    free(current_char);
+                    free(return_token.value);
+                    errMissingOperand();
                 }
-                // Don't change * and /
-                return return_token;
+
+                if (current_char[0] == '+' || current_char[0] == '-') {
+                    if (muldiv) {
+                        free(current_char);
+                        free(return_token.value);
+                        errInvalidOperatorCombination();
+                    }
+
+                    sign = 1;
+                    muldiv = 0;
+                    if (current_char[0] == '-') {
+                        sign *= -1;
+                    }
+                } else {
+                    if (sign) {
+                        free(current_char);
+                        free(return_token.value);
+                        errInvalidOperatorCombination();
+                    }
+                    sign = 0;
+                    if (++muldiv > 1) {
+                        errInvalidOperatorCombination();
+                    }
+                    /*  I'm thinking of just axxing this part
+                        or at least until i get around to
+                        implementing PEMDAS
+                    */
+                }
             }
             (interpreter->pos)++;
             current_char[0] = (interpreter->equation)[interpreter->pos];
-        } while (ispunct((interpreter->equation)[interpreter->pos]));
+        } while (ispunct(current_char[0]) || isspace(current_char[0]));
 
         (interpreter->pos)--;
         if (sign > 0) {
             return_token.value = "+";
         } else if (sign < 0) {
             return_token.value = "-";
-        } else {
-            free(current_char);
-            free(return_token.value);
-            printf("Don't mix and match + - with * /! Also, no mixing * /!\n");
-            exit(1);
         }
 
         free(current_char);
@@ -109,8 +161,13 @@ Token tokenise(Interpreter *interpreter) {
         return return_token;
     }
 
-    printf("I don't recognise that!!\n");
-    exit(1);
+    if(!strcmp(current_char, " ")) {
+        return_token.type = SPACE;
+        free(current_char);
+        return return_token;
+    }
+
+    errUndefinedSymbol();
 }
 
 // DEAL WITH LEAKED MEM!!! //
@@ -122,7 +179,7 @@ double parse(Interpreter *interpreter) {
     while ((interpreter->current).type != END) {
         if ((interpreter->current).type == NUMERICAL) {
             result += atof((interpreter->current).value);
-        } else {
+        } else if ((interpreter->current).type == OPERATION) {
             if (!strcmp((interpreter->current).value, "+")) {
                 (interpreter->pos)++;
                 interpreter->current = tokenise(interpreter);
@@ -134,16 +191,22 @@ double parse(Interpreter *interpreter) {
 
                 result -= atof((interpreter->current).value);
             } else if (!strcmp((interpreter->current).value, "*")) {
-                (interpreter->pos)++;
-                interpreter->current = tokenise(interpreter);
+                do {
+                    (interpreter->pos)++;
+                    interpreter->current = tokenise(interpreter);
+                } while ((interpreter->current).type != NUMERICAL);
 
                 result *= atof((interpreter->current).value);
             } else {
-                (interpreter->pos)++;
-                interpreter->current = tokenise(interpreter);
+                do {
+                    (interpreter->pos)++;
+                    interpreter->current = tokenise(interpreter);
+                } while ((interpreter->current).type != NUMERICAL);
 
                 result /= atof((interpreter->current).value);
             }
+        } else {
+            // This is supposed to be for the brackets
         }
 
         (interpreter->pos)++;
@@ -158,12 +221,20 @@ char *removeSpaces(char *);
 int main() {
     Interpreter test = {.pos = 0};
     char input[100];
-    fgets(input, 100, stdin);
-    test.equation = removeSpaces(input);
-    printf("%lf\n", parse(&test));
+    while (1) {
+        fgets(input, 100, stdin);
+        test.equation = input;
+        printf("%lf\n", parse(&test));
+        test.pos = 0;
+    }
 }
 
 // Returns a str with its space removed
+/*
+    In lieu of the realisation that doing this involves an unnecessary traversal of
+    the equation, I have rewritten the tokeniser to ignore spaces instead.
+    This is kept in case anything goes wrong.
+*/
 char *removeSpaces(char *str) {
     char *new = malloc(strlen(str) * sizeof(char));
     int i = 0;
